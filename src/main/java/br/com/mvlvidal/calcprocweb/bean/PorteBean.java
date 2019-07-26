b9c14c13c97a82ed33f993a5ebd9b9609dfb72bd
mvlvidal <marcusvlvidal@gmail.com>
2019-07-26 15:56:52 -0300

    Implementando Cadastro de Portes

--- src/main/java/br/com/mvlvidal/calcprocweb/bean/PorteBean.java
+++ src/main/java/br/com/mvlvidal/calcprocweb/bean/PorteBean.java
@@ -1,7 +1,9 @@
 package br.com.mvlvidal.calcprocweb.bean;
 
 import br.com.mvlvidal.calcprocweb.dao.PorteDao;
+import br.com.mvlvidal.calcprocweb.model.Edicao;
 import br.com.mvlvidal.calcprocweb.model.Porte;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -9,31 +11,73 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ViewScoped
-@ManagedBean(name="porBean")
-public class PorteBean {
+@ManagedBean(name = "porBean")
+public class PorteBean implements Serializable {
 
     private Porte por1;
     private PorteDao porDao;
     private List<Porte> portes;
-    
+    private boolean editar;
+    private Edicao edicao;
+
     @PostConstruct
-    public void init(){
-    
+    public void init() {
+
         por1 = new Porte();
         porDao = new PorteDao();
         portes = new ArrayList<>();
         portes = porDao.listar();
-        
+        edicao = new Edicao();
+        this.editar = false;
+
     }
-    
-    public void salvar(){
-        
-        Porte por2 = por1;
-        
-        if(por2 != null){
-            porDao.salvar(por1);
+
+    public String salvar() {
+        por1.setEdicao(edicao);
+        Porte por2 = porDao.salvar(por1);
+
+        if (por2 != null) {           
+            por1 = por2;
+            this.editar = false;
+            filtrarEdicao();
+            return "cad-porte";
+        } else {
+            return "cad-porte";
         }
-        
+
+    }
+
+    public String editar(Long id) {
+        if (id != null || id != 0) {
+            this.editar = true;
+            por1 = porDao.find(id);
+            edicao = por1.getEdicao();
+            return "cad-porte";
+        } else {
+            return "";
+        }
+    }
+
+    public String excluir(Long id) {
+        if (id != null || id != 0) {
+            por1 = porDao.deletar(id);
+            filtrarEdicao();
+            return "cad-porte";
+        } else {
+            return "";
+        }
+    }
+
+    public String filtrarEdicao() {
+
+        Edicao edi = edicao;
+        if (edi.getId() == 0) {
+            portes = porDao.listar();
+            return "cad-porte";
+        } else {
+            portes = porDao.listar(edicao.getId());
+            return "cad-porte";
+        }
     }
 
     public Porte getPor1() {
@@ -51,6 +95,17 @@ public class PorteBean {
     public void setPortes(List<Porte> portes) {
         this.portes = portes;
     }
-    
-    
+
+    public boolean isEditar() {
+        return editar;
+    }
+
+    public Edicao getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(Edicao edicao) {
+        this.edicao = edicao;
+    }
+
 }
--- src/main/java/br/com/mvlvidal/calcprocweb/dao/PorteDao.java
+++ src/main/java/br/com/mvlvidal/calcprocweb/dao/PorteDao.java
@@ -1,10 +1,24 @@
 package br.com.mvlvidal.calcprocweb.dao;
 
 import br.com.mvlvidal.calcprocweb.model.Porte;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 public class PorteDao extends GenericDao<Porte, Long>{
  
     public PorteDao(){
         super(Porte.class);
+    }
+    
+    public List<Porte> listar(Long id){
+        
+        Session sessao = br.com.mvlvidal.calcprocweb.dao.HibernateUtil.getSession();
+
+        Criteria criteria = sessao.createCriteria(Porte.class);
+        criteria.add(Restrictions.eq("edicao.id",id));
+              
+        return criteria.list();
     }
 }
